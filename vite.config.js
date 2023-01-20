d46bac45acbe41c1050d388901d2b1df0d700230
Ho Yin (Vis) Wong <viswongdev@gmail.com>
2023-01-20 19:34:04 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default defineConfig({
-    base: '/monthyly-expenditure-visualisation/'
+    base: '/monthyly-expenditure-visualisation/',
+    configureWebpack: (config) => {
+        config.plugins.push(
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: '/assets', to: 'dist/assets' },
+                ],
+            })
+        );
+    },
 });
